24d4203161026618124d811db5e2bd98b1028cb8
Harry Davis <harrytobydavis@gmail.com>
2020-08-03 11:23:04 +0100

    Edited server routes

--- server.js
+++ server.js
@@ -18,14 +18,16 @@ app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
 
-//SETTING UP ROUTES
-app.use('/posts', require('./routes/postRoutes'));
+
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
+} else {
+  //SETTING UP ROUTES
+  app.use('/posts', require('./routes/postRoutes'));
 }
 
 //SETTING UP MONGOOSE
